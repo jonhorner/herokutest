e2e64341854f891bf06bd9e565fa3c2d3580690b
Jon Horner <jon.horner@gmail.com>
2021-04-07 23:03:21 +0100

    Fix update units job

--- app/Jobs/UpdateUnits.php
+++ app/Jobs/UpdateUnits.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
@@ -11,7 +10,7 @@ use Illuminate\Queue\SerializesModels;
 
 use App\Http\Middleware\SwgohHelp;
 
-use App\Http\Models\SwUnitData;
+use App\Models\SwUnitData;
 
 class UpdateUnits implements ShouldQueue
 {
@@ -32,24 +31,23 @@ class UpdateUnits implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle(): void
     {
         $this->getAll();
     }
 
-    private function getAll(){
 
+    private function getAll(): void
+    {
         $units = (new SwgohHelp)->getUnitData();
-        // $processedUnits = [];
 
         foreach ($units as $unit) {
-
             // Filter out event characters
             if(
                 !strpos($unit['baseId'], '_')
                 || in_array($unit['baseId'], array('L3_37','T3_M4'))
             ){
-                $unit = SwUnitData::updateOrCreate(
+                SwUnitData::updateOrCreate(
                     [ 'baseId' => $unit['baseId'] ],
                     [
                         'thumbnailName' => $unit['thumbnailName'],
@@ -58,10 +56,7 @@ class UpdateUnits implements ShouldQueue
                         'combatType' => (int)$unit['combatType']
                     ]
                 );
-
-                // $processedUnits[] = $unit;
             }
         }
-       // return $processedUnits;
     }
 }
