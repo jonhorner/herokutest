786e4d078032db7160e2932f81e4db30298d8b1f
Jon Horner <jon.horner@gmail.com>
2021-04-09 09:34:29 +0100

    Fix google sheets config

--- config/google.php
+++ config/google.php
@@ -20,7 +20,7 @@ return [
     'client_id' => env('GOOGLE_CLIENT_ID', ''),
     'client_secret' => env('GOOGLE_CLIENT_SECRET', ''),
     'redirect_uri' => env('GOOGLE_REDIRECT', ''),
-    'scopes' => [],
+    'scopes' => [\Google_Service_Sheets::DRIVE, \Google_Service_Sheets::SPREADSHEETS],
     'access_type' => 'online',
     'approval_prompt' => 'auto',
 
